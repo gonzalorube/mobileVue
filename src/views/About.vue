0f958667211c5d4ebb9dafae75add70d3dd315ca
gonzalorube <gonzalorube@gmail.com>
2019-08-03 15:06:49 -0300

    add and positioned footer on About

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,19 @@
   <div class="about">
     <Header />
     <div class="subtitle"><h6><em>About us</em></h6></div>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <Footer class="footer"/>
     </div>
 </template>
 
 <script>
   import Header from '../components/layouts/Header.vue';
+  import Footer from '../components/layouts/Footer.vue';
 
   export default {
     components: {
-      Header
+      Header,
+      Footer
     }
   }
 </script>
@@ -19,5 +22,8 @@
 <style scoped>
 .about {
   background-color: #1E1919;
+}
+.footer {
+  padding: 105px 30px 0 30px;
 }
 </style>
